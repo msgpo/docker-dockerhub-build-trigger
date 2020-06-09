0fc5e2b8a15b8a879e12ed8f8fe632a5f7eef325
Robin Östlund <me@robinostlund.name>
2020-06-09 16:02:28 +0200

    initial

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # get environment variables and set default ones
-ENV_DOCKER_IMAGE=${DOCKER_IMAGE:?"Specify a docker image"}
-ENV_TRIGGER_TOKEN=${TRIGGER_TOKEN:?"Specify a trigger token"}
+ENV_TRIGGER_URI=${TRIGGER_URI:?"Specify a trigger uri"}
 ENV_CRONJOB_STRING=${CRONJOB_STRING:?"Specify a cronjob string"}
 
 # set environment variables
 export ENV_DOCKER_IMAGE ENV_TRIGGER_TOKEN ENV_CRONJOB_STRING
 
 # create cronjob
-echo "${ENV_CRONJOB_STRING} /usr/bin/curl --output /dev/null --silent -H \"Content-Type: application/json\" --data '{\"build\": true}' -X POST \"https://registry.hub.docker.com/u/${DOCKER_IMAGE}/trigger/${TRIGGER_TOKEN}/\"" > /etc/crontabs/root
+echo "${ENV_CRONJOB_STRING} /usr/bin/curl --output /dev/null --silent -H \"Content-Type: application/json\" --data '{\"build\": true}' -X POST \"${TRIGGER_URI}\"" > /etc/crontabs/root
 
 # start crond
 bash -c "crond -L /var/log/cron/cron.log && tail -F /var/log/cron/cron.log"
